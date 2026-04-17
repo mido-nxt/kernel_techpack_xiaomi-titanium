2f4b6278247400d2711a7fa06952e07bd412c5e2
Nalinish Ranjan <nalinishranjan05@gmail.com>
2026-04-18 00:21:26 +0530

    techpack: titanium: ps: add tpd_usb_plugin support for Xiaomi Mido

--- touchscreen/focaltech_touch/focaltech_core.h
+++ touchscreen/focaltech_touch/focaltech_core.h
@@ -389,4 +389,10 @@ void fts_ts_trusted_touch_tvm_i2c_failure_report(struct fts_ts_data *fts_data);
 #endif
 #endif
 extern bool gesture_incall;
+
+#ifdef CONFIG_MACH_XIAOMI_MIDO
+extern int set_usb_charge_mode_par;
+void tpd_usb_plugin(bool mode);
+#endif
+
 #endif /* __LINUX_FOCALTECH_CORE_H__ */
